b167366089880c51d1a229318cf6173141bfaff3
MatheusKerscher <kerschermatheus12@gmail.com>
2025-02-05 20:52:18 -0300

    Create picture gallery component

---
+++ src/components/Gallery/Picture/index.jsx
@@ -0,0 +1,22 @@
+const Picture = ({ picture }) => {
+    return(
+        <figure>
+            <img />
+
+            <figcaption>
+                <h4>
+                    {picture.title}
+                </h4>
+
+                <footer>
+                    <p></p>
+
+                    <button></button>
+                    <button></button>
+                </footer>
+            </figcaption>
+        </figure>
+    )
+}
+
+export default Picture
